f76d2bbad887830f0567f06870c663bb0181d9ce
mingxun <mwang87@gmail.com>
2018-06-13 11:28:32 -0700

    showing spinner

--- views.py
+++ views.py
@@ -20,7 +20,6 @@ ALLOWED_EXTENSIONS = set(['mgf', 'mzxml', 'mzml'])
 def homepage():
     response = make_response(render_template('dashboard.html'))
     response.set_cookie('username', str(uuid.uuid4()))
-    response.set_cookie('selectedFiles', "False")
     return response
 
 def allowed_file(filename):
@@ -80,6 +79,25 @@ def upload_3():
 
     return response
 
+def check_ftp_folders(username):
+    url = "ccms-ftp01.ucsd.edu"
+    present_folders = []
+
+    with ftputil.FTPHost(url, credentials.USERNAME, credentials.PASSWORD) as ftp_host:
+        names = ftp_host.listdir(ftp_host.curdir)
+        if not username in names:
+            return present_folders
+
+        ftp_host.chdir(username)
+
+        if "G1" in ftp_host.listdir(ftp_host.curdir):
+            present_folders.append("G1")
+        if "G2" in ftp_host.listdir(ftp_host.curdir):
+            present_folders.append("G2")
+        if "G3" in ftp_host.listdir(ftp_host.curdir):
+            present_folders.append("G3")
+
+    return present_folders
 
 
 def upload_to_gnps(input_filename, folder_for_spectra, group_name):
@@ -102,29 +120,33 @@ def upload_to_gnps(input_filename, folder_for_spectra, group_name):
 @app.route('/analyze', methods=['POST'])
 def analyze():
     username = request.cookies.get('username')
-    if request.cookies.get('selectedFiles') == "False":
-        content = {'status': 'No Files Selected'}
-        return json.dumps(content), 417
 
+    present_folders = check_ftp_folders(username)
+
+    if not "G1" in present_folders:
+        content = {'status': 'Group 1 files required but not selected'}
+        return json.dumps(content), 417
 
     spectra_folder = os.path.join(app.config['UPLOAD_FOLDER'], username)
 
     remote_dir = os.path.join("quickstart_GNPS", username)
-    task_id = launch_GNPS_workflow(remote_dir, "GNPS Quickstart Molecular Networking Analysis ", credentials.USERNAME, credentials.PASSWORD)
+    task_id = launch_GNPS_workflow(remote_dir, "GNPS Quickstart Molecular Networking Analysis ", credentials.USERNAME, credentials.PASSWORD, present_folders)
 
     content = {'status': 'Success', 'task_id': task_id}
     return json.dumps(content), 200
 
 
-def launch_GNPS_workflow(ftp_path, job_description, username, password):
+def launch_GNPS_workflow(ftp_path, job_description, username, password, groups_present):
     invokeParameters = {}
     invokeParameters["workflow"] = "METABOLOMICS-SNETS"
     invokeParameters["protocol"] = "None"
     invokeParameters["desc"] = job_description
     invokeParameters["library_on_server"] = "d.speclibs;"
     invokeParameters["spec_on_server"] = "d." + ftp_path + "/G1;"
-    invokeParameters["spec_on_server_group2"] = "d." + ftp_path + "/G2;"
-    invokeParameters["spec_on_server_group3"] = "d." + ftp_path + "/G3;"
+    if "G2" in groups_present:
+        invokeParameters["spec_on_server_group2"] = "d." + ftp_path + "/G2;"
+    if "G3" in groups_present:
+        invokeParameters["spec_on_server_group3"] = "d." + ftp_path + "/G3;"
     invokeParameters["tolerance.PM_tolerance"] = "2.0"
     invokeParameters["tolerance.Ion_tolerance"] = "0.5"
     invokeParameters["PAIRS_MIN_COSINE"] = "0.70"
@@ -166,6 +188,8 @@ def invoke_workflow(base_url, parameters, login, password):
     r = s.post('https://' + base_url + '/ProteoSAFe/user/login.jsp', data=payload, verify=False)
     r = s.post('https://' + base_url + '/ProteoSAFe/InvokeTools', data=parameters, verify=False)
     task_id = r.text
+
+    print(r.text)
 
     if len(task_id) > 4 and len(task_id) < 60:
         print("Launched Task: : " + r.text)
