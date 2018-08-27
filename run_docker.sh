9ed2d828e48b76e9a0941acd3364310e1fdcb504
Mingxun Wang <mwang87@gmail.com>
2018-08-27 13:56:47 -0700

    quickstart changing ports

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,2 @@
 docker rm gnpsquickstart
-docker run -d -p 5050:5050 --name gnpsquickstart gnpsquickstart /app/run_server.sh
+docker run -d -p 5050:5000 --name gnpsquickstart gnpsquickstart /app/run_server.sh
