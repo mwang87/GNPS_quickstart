a652918ac1c8942dc779210d02d9bff60871eaf1
Mingxun Wang <mwang87@gmail.com>
2018-10-23 14:29:35 -0700

    refactoring out to utils

---
+++ util.py
@@ -0,0 +1,144 @@
+
+ALLOWED_EXTENSIONS = set(['mgf', 'mzxml', 'mzml'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def upload_single_file(request, group):
+    username = request.cookies.get('username')
+
+    filename = ""
+
+    if 'file' not in request.files:
+        return "{}"
+    file = request.files['file']
+    if file.filename == '':
+        return "{}"
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        save_dir = os.path.join(app.config['UPLOAD_FOLDER'], username, group)
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        local_filename = os.path.join(save_dir, filename)
+        file.save(local_filename)
+
+        #Uploading to FTP
+        upload_to_gnps(local_filename, username, group)
+
+        #Remove local file
+        os.remove(local_filename)
+    else:
+        print("not allowed")
+        return "ERROR"
+
+    return json.dumps({"filename": filename})
+
+
+def check_ftp_folders(username):
+    url = "ccms-ftp01.ucsd.edu"
+    present_folders = []
+
+    with ftputil.FTPHost(url, credentials.USERNAME, credentials.PASSWORD) as ftp_host:
+        names = ftp_host.listdir(ftp_host.curdir)
+        if not username in names:
+            return present_folders
+
+        ftp_host.chdir(username)
+
+        if "G1" in ftp_host.listdir(ftp_host.curdir):
+            present_folders.append("G1")
+        if "G2" in ftp_host.listdir(ftp_host.curdir):
+            present_folders.append("G2")
+        if "G3" in ftp_host.listdir(ftp_host.curdir):
+            present_folders.append("G3")
+
+    return present_folders
+
+
+def upload_to_gnps(input_filename, folder_for_spectra, group_name):
+    url = "ccms-ftp01.ucsd.edu"
+
+    with ftputil.FTPHost(url, credentials.USERNAME, credentials.PASSWORD) as ftp_host:
+        names = ftp_host.listdir(ftp_host.curdir)
+        try:
+            if not folder_for_spectra in names:
+                print("MAKING DIR")
+                ftp_host.mkdir(folder_for_spectra)
+        except:
+            print("Cannot Make Folder", folder_for_spectra)
+
+        ftp_host.chdir(folder_for_spectra)
+        try:
+            if not group_name in ftp_host.listdir(ftp_host.curdir):
+                print("MAKING Group DIR")
+                ftp_host.mkdir(group_name)
+        except:
+            print("Cannot Make Folder", group_name)
+        ftp_host.chdir(group_name)
+
+        ftp_host.upload(input_filename, os.path.basename(input_filename))
+
+
+def launch_GNPS_workflow(ftp_path, job_description, username, password, groups_present, email):
+    invokeParameters = {}
+    invokeParameters["workflow"] = "METABOLOMICS-SNETS-V2"
+    invokeParameters["protocol"] = "None"
+    invokeParameters["desc"] = job_description
+    invokeParameters["library_on_server"] = "d.speclibs;"
+    invokeParameters["spec_on_server"] = "d." + ftp_path + "/G1;"
+    if "G2" in groups_present:
+        invokeParameters["spec_on_server_group2"] = "d." + ftp_path + "/G2;"
+    if "G3" in groups_present:
+        invokeParameters["spec_on_server_group3"] = "d." + ftp_path + "/G3;"
+    invokeParameters["tolerance.PM_tolerance"] = "2.0"
+    invokeParameters["tolerance.Ion_tolerance"] = "0.5"
+    invokeParameters["PAIRS_MIN_COSINE"] = "0.70"
+    invokeParameters["MIN_MATCHED_PEAKS"] = "6"
+    invokeParameters["TOPK"] = "10"
+    invokeParameters["CLUSTER_MIN_SIZE"] = "2"
+    invokeParameters["RUN_MSCLUSTER"] = "on"
+    invokeParameters["MAXIMUM_COMPONENT_SIZE"] = "100"
+    invokeParameters["MIN_MATCHED_PEAKS_SEARCH"] = "6"
+    invokeParameters["SCORE_THRESHOLD"] = "0.7"
+    invokeParameters["ANALOG_SEARCH"] = "0"
+    invokeParameters["MAX_SHIFT_MASS"] = "100.0"
+    invokeParameters["FILTER_STDDEV_PEAK_datasetsINT"] = "0.0"
+    invokeParameters["MIN_PEAK_INT"] = "0.0"
+    invokeParameters["FILTER_PRECURSOR_WINDOW"] = "1"
+    invokeParameters["FILTER_LIBRARY"] = "1"
+    invokeParameters["WINDOW_FILTER"] = "1"
+    invokeParameters["CREATE_CLUSTER_BUCKETS"] = "1"
+    invokeParameters["CREATE_ILI_OUTPUT"] = "0"
+    invokeParameters["email"] = email
+    invokeParameters["uuid"] = "1DCE40F7-1211-0001-979D-15DAB2D0B500"
+
+    task_id = invoke_workflow("gnps.ucsd.edu", invokeParameters, username, password)
+
+    return task_id
+
+def invoke_workflow(base_url, parameters, login, password):
+    username = login
+    password = password
+
+    s = requests.Session()
+
+    payload = {
+        'user' : username,
+        'password' : password,
+        'login' : 'Sign in'
+    }
+
+    r = s.post('https://' + base_url + '/ProteoSAFe/user/login.jsp', data=payload, verify=False)
+    r = s.post('https://' + base_url + '/ProteoSAFe/InvokeTools', data=parameters, verify=False)
+    task_id = r.text
+
+    print(r.text)
+
+    if len(task_id) > 4 and len(task_id) < 60:
+        print("Launched Task: : " + r.text)
+        return task_id
+    else:
+        print(task_id)
+        return None
--- views.py
+++ views.py
@@ -9,10 +9,9 @@ import glob
 import json
 import ftputil
 import requests
-
+import util
 import credentials
 
-ALLOWED_EXTENSIONS = set(['mgf', 'mzxml', 'mzml'])
 
 @app.route('/heartbeat', methods=['GET'])
 def heartbeat():
@@ -22,44 +21,13 @@ def heartbeat():
 def homepage():
     response = make_response(render_template('dashboard.html'))
     response.set_cookie('username', str(uuid.uuid4()))
+    response.set_cookie('selectedFiles', "False")
+
     return response
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-def upload_single_file(request, group):
-    username = request.cookies.get('username')
-
-    filename = ""
-
-    if 'file' not in request.files:
-        return "{}"
-    file = request.files['file']
-    if file.filename == '':
-        return "{}"
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        save_dir = os.path.join(app.config['UPLOAD_FOLDER'], username, group)
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-        local_filename = os.path.join(save_dir, filename)
-        file.save(local_filename)
-
-        #Uploading to FTP
-        upload_to_gnps(local_filename, username, group)
-
-        #Remove local file
-        os.remove(local_filename)
-    else:
-        print("not allowed")
-        return "ERROR"
-
-    return json.dumps({"filename": filename})
 
 @app.route('/upload1', methods=['POST'])
 def upload_1():
-    upload_string = upload_single_file(request, "G1")
+    upload_string = util.upload_single_file(request, "G1")
     response = make_response(render_template('dashboard.html'))
     response.set_cookie('selectedFiles', "True")
 
@@ -67,7 +35,7 @@ def upload_1():
 
 @app.route('/upload2', methods=['POST'])
 def upload_2():
-    upload_string = upload_single_file(request, "G2")
+    upload_string = util.upload_single_file(request, "G2")
     response = make_response(render_template('dashboard.html'))
     response.set_cookie('selectedFiles', "True")
 
@@ -75,55 +43,11 @@ def upload_2():
 
 @app.route('/upload3', methods=['POST'])
 def upload_3():
-    upload_string = upload_single_file(request, "G3")
+    upload_string = util.upload_single_file(request, "G3")
     response = make_response(render_template('dashboard.html'))
     response.set_cookie('selectedFiles', "True")
 
     return response
-
-def check_ftp_folders(username):
-    url = "ccms-ftp01.ucsd.edu"
-    present_folders = []
-
-    with ftputil.FTPHost(url, credentials.USERNAME, credentials.PASSWORD) as ftp_host:
-        names = ftp_host.listdir(ftp_host.curdir)
-        if not username in names:
-            return present_folders
-
-        ftp_host.chdir(username)
-
-        if "G1" in ftp_host.listdir(ftp_host.curdir):
-            present_folders.append("G1")
-        if "G2" in ftp_host.listdir(ftp_host.curdir):
-            present_folders.append("G2")
-        if "G3" in ftp_host.listdir(ftp_host.curdir):
-            present_folders.append("G3")
-
-    return present_folders
-
-
-def upload_to_gnps(input_filename, folder_for_spectra, group_name):
-    url = "ccms-ftp01.ucsd.edu"
-
-    with ftputil.FTPHost(url, credentials.USERNAME, credentials.PASSWORD) as ftp_host:
-        names = ftp_host.listdir(ftp_host.curdir)
-        try:
-            if not folder_for_spectra in names:
-                print("MAKING DIR")
-                ftp_host.mkdir(folder_for_spectra)
-        except:
-            print("Cannot Make Folder", folder_for_spectra)
-
-        ftp_host.chdir(folder_for_spectra)
-        try:
-            if not group_name in ftp_host.listdir(ftp_host.curdir):
-                print("MAKING Group DIR")
-                ftp_host.mkdir(group_name)
-        except:
-            print("Cannot Make Folder", group_name)
-        ftp_host.chdir(group_name)
-
-        ftp_host.upload(input_filename, os.path.basename(input_filename))
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
@@ -132,7 +56,7 @@ def analyze():
     if len(email) < 1 or len(email) > 100:
         email = "ccms.web@gmail.com"
 
-    present_folders = check_ftp_folders(username)
+    present_folders = util.check_ftp_folders(username)
 
     if not "G1" in present_folders:
         content = {'status': 'Group 1 files required but not selected'}
@@ -141,70 +65,7 @@ def analyze():
     spectra_folder = os.path.join(app.config['UPLOAD_FOLDER'], username)
 
     remote_dir = os.path.join("quickstart_GNPS", username)
-    task_id = launch_GNPS_workflow(remote_dir, "GNPS Quickstart Molecular Networking Analysis ", credentials.USERNAME, credentials.PASSWORD, present_folders, email)
+    task_id = util.launch_GNPS_workflow(remote_dir, "GNPS Quickstart Molecular Networking Analysis ", credentials.USERNAME, credentials.PASSWORD, present_folders, email)
 
     content = {'status': 'Success', 'task_id': task_id}
     return json.dumps(content), 200
-
-
-def launch_GNPS_workflow(ftp_path, job_description, username, password, groups_present, email):
-    invokeParameters = {}
-    invokeParameters["workflow"] = "METABOLOMICS-SNETS-V2"
-    invokeParameters["protocol"] = "None"
-    invokeParameters["desc"] = job_description
-    invokeParameters["library_on_server"] = "d.speclibs;"
-    invokeParameters["spec_on_server"] = "d." + ftp_path + "/G1;"
-    if "G2" in groups_present:
-        invokeParameters["spec_on_server_group2"] = "d." + ftp_path + "/G2;"
-    if "G3" in groups_present:
-        invokeParameters["spec_on_server_group3"] = "d." + ftp_path + "/G3;"
-    invokeParameters["tolerance.PM_tolerance"] = "2.0"
-    invokeParameters["tolerance.Ion_tolerance"] = "0.5"
-    invokeParameters["PAIRS_MIN_COSINE"] = "0.70"
-    invokeParameters["MIN_MATCHED_PEAKS"] = "6"
-    invokeParameters["TOPK"] = "10"
-    invokeParameters["CLUSTER_MIN_SIZE"] = "2"
-    invokeParameters["RUN_MSCLUSTER"] = "on"
-    invokeParameters["MAXIMUM_COMPONENT_SIZE"] = "100"
-    invokeParameters["MIN_MATCHED_PEAKS_SEARCH"] = "6"
-    invokeParameters["SCORE_THRESHOLD"] = "0.7"
-    invokeParameters["ANALOG_SEARCH"] = "0"
-    invokeParameters["MAX_SHIFT_MASS"] = "100.0"
-    invokeParameters["FILTER_STDDEV_PEAK_datasetsINT"] = "0.0"
-    invokeParameters["MIN_PEAK_INT"] = "0.0"
-    invokeParameters["FILTER_PRECURSOR_WINDOW"] = "1"
-    invokeParameters["FILTER_LIBRARY"] = "1"
-    invokeParameters["WINDOW_FILTER"] = "1"
-    invokeParameters["CREATE_CLUSTER_BUCKETS"] = "1"
-    invokeParameters["CREATE_ILI_OUTPUT"] = "0"
-    invokeParameters["email"] = email
-    invokeParameters["uuid"] = "1DCE40F7-1211-0001-979D-15DAB2D0B500"
-
-    task_id = invoke_workflow("gnps.ucsd.edu", invokeParameters, username, password)
-
-    return task_id
-
-def invoke_workflow(base_url, parameters, login, password):
-    username = login
-    password = password
-
-    s = requests.Session()
-
-    payload = {
-        'user' : username,
-        'password' : password,
-        'login' : 'Sign in'
-    }
-
-    r = s.post('https://' + base_url + '/ProteoSAFe/user/login.jsp', data=payload, verify=False)
-    r = s.post('https://' + base_url + '/ProteoSAFe/InvokeTools', data=parameters, verify=False)
-    task_id = r.text
-
-    print(r.text)
-
-    if len(task_id) > 4 and len(task_id) < 60:
-        print("Launched Task: : " + r.text)
-        return task_id
-    else:
-        print(task_id)
-        return None
