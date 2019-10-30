3d1011a1c7ffd9367aa5ede9c5c59dd619b69a7a
Mingxun Wang <mwang87@gmail.com>
2019-10-29 17:51:13 -0700

    adding more validators

---
+++ test/test_validators.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import sys
+sys.path.insert(0, "..")
+
+def test_datat1_failure():
+    import batch_validator
+    passes_validation, failures, errors_list, valid_rows, total_rows = batch_validator.perform_batch_validation("reference_data/bile_acid_ref_failure.tsv")
+    assert(passes_validation == False)
+
+def test_datat2():
+    import batch_validator
+    passes_validation, failures, errors_list, valid_rows, total_rows = batch_validator.perform_batch_validation("reference_data/GNPS00001_output_batch.tsv")
+    assert(passes_validation == True)
+
+    passes_validation, failures, errors_list, valid_rows, total_rows = batch_validator.perform_batch_validation("reference_data/GNPS00002_output_batch.tsv")
+    assert(passes_validation == True)
+
+    passes_validation, failures, errors_list, valid_rows, total_rows = batch_validator.perform_batch_validation("reference_data/GNPS00003_output_batch.tsv")
+    assert(passes_validation == False)
+
+    passes_validation, failures, errors_list, valid_rows, total_rows = batch_validator.perform_batch_validation("reference_data/GNPS00004_output_batch.tsv")
+    assert(passes_validation == False)
+
+    passes_validation, failures, errors_list, valid_rows, total_rows = batch_validator.perform_batch_validation("reference_data/GNPS00005_output_batch.tsv")
+    assert(passes_validation == False)
+
+    passes_validation, failures, errors_list, valid_rows, total_rows = batch_validator.perform_batch_validation("reference_data/GNPS00006_output_batch.tsv")
+    assert(passes_validation == False)
