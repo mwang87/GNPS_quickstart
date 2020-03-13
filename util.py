fd20ffa41189985a9f432add16829daa06af70dc
Mingxun Wang <mwang87@gmail.com>
2020-03-13 09:46:31 -0700

    pinning to version

--- util.py
+++ util.py
@@ -109,6 +109,7 @@ def get_classic_networking_lowres_parameters():
     invokeParameters = {}
     invokeParameters["workflow"] = "METABOLOMICS-SNETS-V2"
     invokeParameters["protocol"] = "None"
+    invokeParameters["workflow_version"] = "release_18"
     invokeParameters["library_on_server"] = "d.speclibs;"
     invokeParameters["tolerance.PM_tolerance"] = "2.0"
     invokeParameters["tolerance.Ion_tolerance"] = "0.5"
@@ -138,6 +139,7 @@ def get_classic_networking_highres_parameters():
     invokeParameters = {}
     invokeParameters["workflow"] = "METABOLOMICS-SNETS-V2"
     invokeParameters["protocol"] = "None"
+    invokeParameters["workflow_version"] = "release_18"
     invokeParameters["library_on_server"] = "d.speclibs;"
     invokeParameters["tolerance.PM_tolerance"] = "0.05"
     invokeParameters["tolerance.Ion_tolerance"] = "0.05"
@@ -221,6 +223,7 @@ def get_featurenetworking_lowres_parameters():
     invokeParameters = {}
     invokeParameters["workflow"] = "FEATURE-BASED-MOLECULAR-NETWORKING"
     invokeParameters["protocol"] = "None"
+    invokeParameters["workflow_version"] = "release_18"
     invokeParameters["desc"] = "Job Description"
     invokeParameters["library_on_server"] = "d.speclibs;"
 
