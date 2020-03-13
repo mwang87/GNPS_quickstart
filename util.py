418c61c3b8de572a1c67f4f9921279840fc99a4a
Mingxun Wang <mwang87@gmail.com>
2020-03-13 11:30:31 -0700

    adding further fixes

--- util.py
+++ util.py
@@ -267,6 +267,7 @@ def get_featurenetworking_highres_parameters():
     invokeParameters = {}
     invokeParameters["workflow"] = "FEATURE-BASED-MOLECULAR-NETWORKING"
     invokeParameters["protocol"] = "None"
+    invokeParameters["workflow_version"] = "release_18"
     invokeParameters["desc"] = "Job Description"
     invokeParameters["library_on_server"] = "d.speclibs;"
 
